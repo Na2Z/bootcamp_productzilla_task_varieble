7084b437d070d474965157999f5e67d58fd99018
Shahnaz Ferdiansyah <113977528+Na2Z@users.noreply.github.com>
2023-02-28 20:09:34 +0700

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,26 @@
+console.log("Tugas Prakterk");
+//1. Buatlah 5 variebel dengan 3 perhitungan (angka)
+//2. Buatlah masing masing 2 dari masing masing operator perbandingan
+//outputkan hasilnya.
+
+console.log("Jawab No 1");
+let x1 = 5 * 3 + 10;
+let x2 = 180 / 3 ** 3;
+let x3 = x1 * 4 + 100;
+let x4 = x3 / 20 - 500;
+let x5 = x4 ** 100 * 0;
+
+console.log(x1);
+console.log(x2);
+console.log(x3);
+console.log(x4);
+console.log(x5);
+
+console.log("Jawab No 2");
+
+var a = 3;
+var b = -10;
+
+console.log(a > b);
+console.log(a ** 2 > b ** 2);
+console.log(a > b ** 2);
