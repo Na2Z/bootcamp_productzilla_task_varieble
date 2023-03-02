70f6da5aa290008ca72bf2a50b2239ba237b553e
Na2Z <20102169@ittelkom-pwt.ac.id>
2023-03-02 19:32:26 +0700

    Check

--- JS.js
+++ JS.js
@@ -24,3 +24,72 @@ var b = -10;
 console.log(a > b);
 console.log(a ** 2 > b ** 2);
 console.log(a > b ** 2);
+
+//Home Task
+// 1. Buatlah 20 variable yang dipakai untuk menghitung 5 angka, outputkan hasilnya
+// 2. buat masing masing 10 dari setiap operator perbandingan, outputkan hasilnya
+// 3. simpan di github dengan format repository bootcamp_productzilla_take_home_task_variable
+
+// Jawab No 1
+console.log("jawab No 1 Home Task");
+let a1 = 10 + (25 * 2) / 5 ** 2;
+let a2 = 3 + (7 * 3) / 21 ** 10;
+let a3 = 20 + (45 * 6) / 7 ** 3;
+let a4 = 30 + (10 * 3) / 5 ** 1;
+let a5 = 50 + (60 * 1) / 2 / 4 / 2 ** 2;
+let a6 = 10 + (25 * 2) / 5 ** 0;
+let a7 = 1 + (2 * 2) / 10 ** 3;
+let a8 = 3 + (20 * 2) / 5 ** 10;
+let a9 = 20 + (35 * 2) / 5 ** 2;
+let a10 = 100 + (25 * 2) / 50 ** 1;
+let a11 = 30 / 3 + 10 * 10 ** 3;
+let a12 = 100 / 20 + 100 * 20 ** 10;
+let a13 = 60 / 30 + 5 * 6 ** 1;
+let a14 = 30 / 3 + 10 * 10 ** 3;
+let a15 = 30 / 3 + 10 * 40 ** 4;
+let a16 = 10 / 3 + 50 * 10 ** 0;
+let a17 = 50 / 2 + 3 * 15 ** 10;
+let a18 = 30 / 3 + 10 * 10 ** 3;
+let a19 = 10 / 2 + 23 * 100 ** 0;
+let a20 = 100 / 1000 + 100 * 10 ** 1;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+console.log(a6);
+console.log(a7);
+console.log(a8);
+console.log(a9);
+console.log(a10);
+console.log(a11);
+console.log(a12);
+console.log(a13);
+console.log(a14);
+console.log(a15);
+console.log(a16);
+console.log(a17);
+console.log(a18);
+console.log(a19);
+console.log(a20);
+
+console.log("Jawab No 2 Home Task");
+
+let x = 3;
+let y = 10;
+let z = 5;
+
+console.log(x < y);
+console.log(x < z);
+console.log(y < z);
+console.log(y < x);
+console.log(z < x);
+console.log(z < y);
+
+console.log(x >= y);
+console.log(x >= z);
+console.log(y >= z);
+console.log(y >= x);
+console.log(z >= x);
+console.log(z >= y);
